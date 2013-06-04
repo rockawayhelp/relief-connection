672e56039ecc5cc24e37c1a6af0b41774ed9cd42
Steve Kinney <hello@stevekinney.net>
2013-06-03 21:17:18 -0400

    Install packages; ignore files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var http = require('http'),
-    url = require('url');
+    url = require('url'),
+    qs = require('querystring'),
+    geocoder = require('geocoder');
 
 var server = http.createServer(function (request, response) {
   if ( request.url === "/need" ) {
@@ -10,16 +12,24 @@ var server = http.createServer(function (request, response) {
 });
 
 function processNeed (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("You have a need.");
+  geocoder.geocode("222 Beach 100th Street, 11694", function ( err, data ) {
+    if (err) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.end("Something went wrong.")
+    } else {
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.end(JSON.stringify(data));
+    }
+  });
 }
 
 function notFound (request, response) {
   console.log("404: " + request.url);
   response.writeHead(404, {"Content-Type": "text/plain"});
-  response.end("Page not found.");
+  response.end("Resource not found.");
 }
 
-server.listen(3000);
+var port = process.env.PORT || 3000;
 
-console.log("St. Louis is Listening on Port 3000.");
+server.listen(port);
+console.log("St. Louis is Listening on Port " + port + ".");
