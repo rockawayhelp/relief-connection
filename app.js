e7e6cdbc9172d2e8af2b1ea1f0110d7ceebd4820
Steve Kinney <hello@stevekinney.net>
2013-06-05 22:30:27 -0400

    Inspect posted data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http')
   , qs = require('querystring')
+  , util = require('util')
   , twilio = require('twilio');
 
 var server = http.createServer(function (request, response) {
@@ -9,7 +10,7 @@ var server = http.createServer(function (request, response) {
   });
   
   request.on('end', function () {
-    console.log('Posted: ' + qs.parse(body));
+    console.log('Posted: ' + util.inspect(qs.parse(body)));
     response.writeHead(200);
     response.end("Hello world");
   });
