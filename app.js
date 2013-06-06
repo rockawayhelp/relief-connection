ca2af30cfef91dfeb7dd31e028b59b9ac68776dc
Steve Kinney <hello@stevekinney.net>
2013-06-05 21:44:02 -0400

    Flesh out structure; setup Heroku

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 var http = require('http'),
-    url = require('url'),
-    qs = require('querystring'),
     geocoder = require('geocoder');
 
 var server = http.createServer(function (request, response) {
-  if ( request.url === "/need" ) {
-    processNeed(request, response);
-  } else {
-    notFound(request, response);
-  }
+  response.writeHead(200, {"Content-Type": "application/json"});
+  response.end("Hello world.");
 });
-
-function processNeed (request, response) {
-  geocoder.geocode("222 Beach 100th Street, 11694", function ( err, data ) {
-    if (err) {
-      response.writeHead(500, {"Content-Type": "text/plain"});
-      response.end("Something went wrong.")
-    } else {
-      response.writeHead(200, {"Content-Type": "application/json"});
-      response.end(JSON.stringify(data));
-    }
-  });
-}
-
-function notFound (request, response) {
-  console.log("404: " + request.url);
-  response.writeHead(404, {"Content-Type": "text/plain"});
-  response.end("Resource not found.");
-}
 
 var port = process.env.PORT || 3000;
 
